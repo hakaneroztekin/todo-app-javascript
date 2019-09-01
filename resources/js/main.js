0d474b99aca6ceffdff7cb65ba9ae31e1f0205ff
hakaneroztekin <h.eroztekin@gmail.com>
2019-09-01 13:30:22 +0300

    prepare for adding items to the todo list

---
+++ resources/js/main.js
@@ -0,0 +1,9 @@
+
+// User clicked on the add button
+// If there is any text inside the item field, add that text to the todo list
+document.getElementById('add').addEventListener('click', function () {
+    let value = document.getElementById('item').value;
+    if(value) {
+
+    }
+});
