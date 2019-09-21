17558073ae424fad2e4101a96561ff83b7470ef0
eroztekin <eroztekin@itu.edu.tr>
2019-09-21 08:08:08 +0300

    update env var

--- config.js
+++ config.js
@@ -3,9 +3,9 @@
 // console.log(require('dotenv').config({path: __dirname + '/.env'}));
 require('dotenv').config({path: __dirname + '/.env'});
 module.exports = {
-    host : process.env.HOST,
-    port : process.env.PORT,
-    database : process.env.DATABASE,
+    host : process.env.DB_HOST,
+    port : process.env.DB_PORT,
+    database : process.env.DB_DATABASE,
     username : process.env.DB_USERNAME,
     password : process.env.DB_PASSWORD
 };
